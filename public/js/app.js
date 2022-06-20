da0005fdd3db2d55579063bf72c68eae7e2e8737
ariannagreco3393 <a.greco3393@gmail.com>
2022-06-20 16:20:11 +0200

    add routes

--- public/js/app.js
+++ public/js/app.js
@@ -27654,8 +27654,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\qwerty\Desktop\Classe60\laravel\laravel-base-crud\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\qwerty\Desktop\Classe60\laravel\laravel-base-crud\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/LARAVEL/comics-team/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/LARAVEL/comics-team/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PageController@index')->name('app');
 
-Route::get('/', function(){
+Route::get('/', function () {
     return view('comics.home');
 })->name('home');
 
@@ -25,3 +25,21 @@ Route::delete('/comics/{comic}/', 'ComicsController@destroy')->name('comics.dest
 
 
 //Route::resource('/comics', 'ComicsController');
+
+
+
+//ROTTE PER CHARACTERS (CRUD)
+// INDEX
+Route::get('/characters', 'charactersController@index')->name('characters.index');
+// CREATE
+Route::get('/characters/create', 'charactersController@create')->name('characters.create');
+// STORE
+Route::post('/characters', 'charactersController@store')->name('characters.store');
+// SHOW
+Route::get('/characters/{character}', 'charactersController@show')->name('characters.show');
+// EDIT
+Route::get('/characters/{character}/edit', 'charactersController@edit')->name('characters.edit');
+// UPDATE
+Route::put('/characters/{character}', 'charactersController@update')->name('characters.update');
+// DESTROY
+Route::delete('/characters/{character}/', 'charactersController@destroy')->name('characters.destroy');
